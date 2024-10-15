a44923ab7b9023e14bc33dd39537ed42b09c07f4
rahulosp <55977970+rahulosp@users.noreply.github.com>
2024-10-15 18:03:02 +0530

    Update next.config.js rahosp

--- site/next.config.js
+++ site/next.config.js
@@ -2,10 +2,10 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'ATX Defense',
+    description: 'The ATX Defense store for Kasm workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://github.com/rahulosp/kasm-registry/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
